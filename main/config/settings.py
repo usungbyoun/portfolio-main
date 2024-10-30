58bca0a2659cf7d32ea3f723153bf66f4715c07d
usung <usung418@naver.com>
2024-10-30 22:57:59 +0900

    241030

--- main/config/settings.py
+++ main/config/settings.py
@@ -25,9 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+# production
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+# develope
+# DEBUG = True
+# ALLOWED_HOSTS = ['*']
+
+
 # Application definition
 
 INSTALLED_APPS = [
